2922d2a3df87a6d2e07492a93e7154c8120b4d55
omarx33 <42531886+omarx33@users.noreply.github.com>
2020-04-06 03:52:49 -0500

    codeigniter 4.0.2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,8 @@ class Home extends BaseController
 {
 	public function index()
 	{
-//		return view('welcome_message');
-echo "hola";
+		return view('welcome_message');
+//echo "hola";
 	}
 
 	//--------------------------------------------------------------------
